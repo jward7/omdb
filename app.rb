cda24b10bfc63facd62b6b25f9e9b64ab56ea4b9
Justin Ward <jward@businest.com>
2015-04-08 23:21:25 +1000

    deleted files. got li working. Need to add movie result line route

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pg' # for postgresql database
-require 'pry'
+#require 'pry' not installed on desktop
 require 'httparty'
 require 'uri'
 
@@ -15,86 +15,30 @@ end
 post '/movie' do
   url = "http://www.omdbapi.com/?s=#{params[:movie_name]}"
 
+
   #check database first. If movie exist then fetch from database else fetch from omdb
-  db = PG.connect(:host=> 'localhost', :user => 'postgres', :password => 'postgres', :dbname => 'omdb')
+  db = PG.connect(:host=> 'localhost', :user => 'postgres', :password => 'drexler69', :dbname => 'omdb')
   rows = db.exec("SELECT * FROM movies WHERE Title = '#{params[:movie_name]}'")
 
   if rows.count > 0 # movie exists in database
     @result = rows.first
-    @multi = @result
-    #raise 'found in database'
     @source = 'local database'
   else # fetch from OMDB
-    raw_result = HTTParty.get(URI.escape(url)) # ("http://www.omdbapi.com/?t=rocky&y=&plot=short&r=json")
+    raw_result = HTTParty.get(URI.escape(url))
     @source = 'OMDb'
-    @url = url
 
-    @multi = @result
     @result = {}
     raw_result.each do |key, value|
       @result[key.downcase] = value
     end
+
+    @title_year = @result['search'].collect {|x| "#{x['Title']}, #{x['Year']}"}
     # save to database
-    sql = "INSERT INTO movies (Title, Year, Poster) VALUES ('#{ @result['title'] }', '#{ @result['year'] }', '#{ @result['poster'] }');"
-    db.exec(sql)
+    #sql = "INSERT INTO movies (Title, Year, Poster) VALUES ('#{ @result['title'] }', '#{ @result['year'] }', '#{ @result['poster'] }');"
+    #db.exec(sql)
   end
 
   db.close
-  erb :json_view
-end
-
-get '/movies/new' do
-  erb :new
-end
-
-get '/movies/:id/edit' do
-  sql = "SELECT * FROM movies WHERE id = #{params[:id]}"
-  rows = run_sql(sql)
-  @movie = rows[0]
-  erb :edit
-end
-
-=begin
-#create new movie
-post '/movies' do
-  #binding.pry
-  sql = "INSERT INTO movies (title,image,url) VALUES ('#{params['title']}', '#{params['image_url']}')"
-
-  run_sql(sql)
-  redirect to '/'
-end
-=end
-
-#update existing movie
-=begin
-post '/movies/:id' do
-
-  sql = "UPDATE movies SET name='#{params[:name]}', image_url='#{params[:image_url]}' WHERE id = #{params[:id]};"
-  run_sql(sql)
-  redirect to '/'
-end
-
-get 'movies/:id/delete' do
-  sql = "DELETE from movies WHERE id = #{params[:id]}"
-
-  run_sql(sql)
-  redirect to '/'
-end
-=end
-
-get '/search' do
-  erb :search
-end
-
-post '/search' do
-  # need to formulate the link below
-  # handle if title has spaces
-  title_plus = params[:'title'].tr(" ","+")
-  construct_search_url = "http://www.omdbapi.com/?t=#{title_plus}&plot=short&r=json"
-  construct_search_url = URI.escape(construct_search_url)
-  @omdb_movie_search = HTTParty.get construct_search_url # ("http://www.omdbapi.com/?t=rocky&y=&plot=short&r=json")
-  #redirect to '/search'
-  @p = title_plus
   erb :movie
 end
 
@@ -104,7 +48,7 @@ end
 
 def run_sql(sql)
   #db = PG.connect(:dbname => 'omdb')
-  db = PG.connect(:host=> 'localhost', :user => 'postgres', :password => 'postgres', :dbname => 'omdb')
+  db = PG.connect(:host=> 'localhost', :user => 'postgres', :password => 'drexler69', :dbname => 'omdb')
   @rows = db.exec(sql)
   db.close
   return rows
---
+++ json.rb
@@ -0,0 +1,64 @@
+{
+    Search: [
+        {
+            Title: "Speed",
+            Year: "1994",
+            imdbID: "tt0111257",
+            Type: "movie"
+        },
+        {
+            Title: "Need for Speed",
+            Year: "2014",
+            imdbID: "tt2369135",
+            Type: "movie"
+        },
+        {
+            Title: "Speed 2: Cruise Control",
+            Year: "1997",
+            imdbID: "tt0120179",
+            Type: "movie"
+        },
+        {
+            Title: "Speed Racer",
+            Year: "2008",
+            imdbID: "tt0811080",
+            Type: "movie"
+        },
+        {
+            Title: "Need for Speed: Most Wanted",
+            Year: "2005",
+            imdbID: "tt0462448",
+            Type: "game"
+        },
+        {
+            Title: "Mother, Jugs & Speed",
+            Year: "1976",
+            imdbID: "tt0074923",
+            Type: "movie"
+        },
+        {
+            Title: "Speed Scandal",
+            Year: "2008",
+            imdbID: "tt1360795",
+            Type: "movie"
+        },
+        {
+            Title: "Need for Speed: Carbon",
+            Year: "2006",
+            imdbID: "tt0790814",
+            Type: "game"
+        },
+        {
+            Title: "Exit Speed",
+            Year: "2008",
+            imdbID: "tt1042497",
+            Type: "movie"
+        },
+        {
+            Title: "The Wizard of Speed and Time",
+            Year: "1988",
+            imdbID: "tt0081766",
+            Type: "movie"
+        }
+    ]
+}
